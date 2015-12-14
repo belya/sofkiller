cd09983e213a2cf41bbffc42184225e28d7a5d36
BlackMoon <belchikov1996@mail.ru>
2015-12-14 18:44:40 +0300

    Fixed some troubles with users scope

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
-  load_and_authorize_resource 
+  load_and_authorize_resource
   
   def index
-    @users = User.all
+    @users = User.accessible_by(current_ability) 
   end
 
   def show
